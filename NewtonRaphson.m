32f2399be2bfb66c72374bedb0b005a43004d7d9
DivjotSingh24 <146051571+DivjotSingh24@users.noreply.github.com>
2024-11-29 08:36:13 +0530

    Update NewtonRaphson.m

--- NewtonRaphson.m
+++ NewtonRaphson.m
@@ -1,32 +1,27 @@
-format long
-clear all
-clc
-syms x
-f = @(x) x * log10(x) - 1.2;
-df=diff(f,x);
-dfx = @(x) df;
-x0=1.5;
-n=4;
-Variables = {'Iter', 'x', 'f_x0', 'Error'};
-iter=1;
-err=abs(f(x0));
-epsilon=5*10^(-n-1);
-itermax=70;
-HG=[];
-if dfx(x0)<10^(-9)
-    disp('Wrong choice of initial guess');
-else
-    while (iter<=itermax && err>epsilon)
-        x1= x0-f(x0)/dfx(x0);
-        err = abs(f(x0));
-        HG=[HG;iter x0 f(x0) err];
-        iter=iter+1;
-        x0=x1;
-    end
+clear all;
+x0 = input('Enter initial guess: ');
+e = input('Enter error margin: ');
+syms x;
+func = x * log10(x) - 1.2; % Corrected function definition
+f = matlabFunction(func); % Used `matlabFunction` to create a numerical function handle
+d = diff(func); % Differentiated the symbolic function
+df = matlabFunction(d); % Used `matlabFunction` for the derivative function handle
+
+% Check if derivative at the initial guess is zero
+if abs(df(x0)) == 0
+    fprintf('Newton Raphson method is inapplicable\n');
+    return;
 end
-disp('Output Table with Iteration wise')
-Result= array2table(HG);
-Result.Properties.VariableNames(1:size(HG,2)) = Variables;
-x0= x0-rem(x0,10^-n);
-fprintf('Converged solution after %d iterations \n', iter);
-fprintf('Root is %2.5f \n', x0)
+
+x1 = x0 - (f(x0) / df(x0)); % Initial step
+i = 1; % Iteration counter
+
+while abs(x1 - x0) > e
+    x0 = x1; % Update x0 to the latest value
+    x1 = x0 - (f(x0) / df(x0)); % Compute the next iteration
+    i = i + 1; % Increment iteration counter
+end
+
+% Display the results
+fprintf('The root is: %f\n', x0);
+fprintf('No of iterations is %d\n', i);
